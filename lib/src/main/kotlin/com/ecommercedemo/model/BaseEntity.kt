ba02eee7457159f4042dfdb224cdb14fa604140f
aron <vaupelaron@gmail.com>
2024-10-03 16:33:02 +0700

    Refactor CustomProperty and BaseEntity to enable custom  properties on the BaseEntity level

--- lib/src/main/kotlin/com/ecommercedemo/model/BaseEntity.kt
+++ lib/src/main/kotlin/com/ecommercedemo/model/BaseEntity.kt
@@ -1,12 +1,14 @@
 package com.ecommercedemo.model
 
+import com.ecommercedemo.model.embedded.CustomProperty
 import jakarta.persistence.*
 import java.time.LocalDateTime
 import java.util.UUID
+import kotlin.reflect.KClass
 
 
 @MappedSuperclass
-@Suppress("unused")
+@Suppress("unused", "JpaQlInspection", "JpaDataSourceORMInspection")
 abstract class BaseEntity {
 
     @Id
@@ -18,6 +20,19 @@ abstract class BaseEntity {
 
     @Column(nullable = false)
     var updatedAt: LocalDateTime = LocalDateTime.now()
+
+    @ElementCollection
+    @CollectionTable(name = "custom_properties", joinColumns = [JoinColumn(name = "base_entity_id")])
+    @Column(name = "custom_property", columnDefinition = "jsonb")
+    lateinit var customProperties: MutableSet<CustomProperty>
+
+    fun addCustomProperty(property: CustomProperty) {
+        customProperties.add(property)
+    }
+
+    fun getCustomPropertiesForEntity(entity: KClass<*>): List<CustomProperty> {
+        return customProperties.filter { it.entity == entity.qualifiedName }
+    }
 
     @PrePersist
     fun onCreate() {
--- lib/src/main/kotlin/com/ecommercedemo/model/embedded/CustomProperty.kt
+++ lib/src/main/kotlin/com/ecommercedemo/model/embedded/CustomProperty.kt
@@ -1,14 +1,27 @@
 package com.ecommercedemo.model.embedded
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.persistence.Embeddable
 import jakarta.validation.constraints.NotBlank
+import kotlin.reflect.KClass
 
 @Embeddable
 @Suppress("unused")
-class CustomProperty<T>(
+class CustomProperty(
+    @field:NotBlank(message = "Key is mandatory")
+    val entity: String,
     @field:NotBlank(message = "Key is mandatory")
     val key: String,
-    val value: T?
+    val value: String?
 ) {
-    constructor() : this("", null)
+    constructor() : this("", "", null)
+
+    inline fun <reified T> deserialize(): T? {
+        return value?.let { ObjectMapper().readValue(it, T::class.java) }
+    }
+
+    fun <T> serialize(key: String, value: T, entity: KClass<*>): CustomProperty {
+        return CustomProperty(key, ObjectMapper().writeValueAsString(value), entity.qualifiedName ?: "")
+    }
+
 }
