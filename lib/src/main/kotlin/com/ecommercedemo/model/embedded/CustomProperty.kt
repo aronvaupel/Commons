bd48bdc6e66fbb5d84aa9cec71e470520072013a
aron <vaupelaron@gmail.com>
2024-10-03 15:29:46 +0700

    Simplyfy CustomProperty

--- lib/src/main/kotlin/com/ecommercedemo/model/embedded/CustomProperty.kt
+++ lib/src/main/kotlin/com/ecommercedemo/model/embedded/CustomProperty.kt
@@ -1,25 +1,14 @@
 package com.ecommercedemo.model.embedded
 
 import jakarta.persistence.Embeddable
-import jakarta.persistence.Column
 import jakarta.validation.constraints.NotBlank
-import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.databind.ObjectMapper
 
 @Embeddable
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Suppress("unused")
 class CustomProperty<T>(
-    @field:NotBlank(message = "Label is mandatory")
-    @Column(name = "label")
-    val label: String,
-
-    @Column(name = "value", columnDefinition = "jsonb")
+    @field:NotBlank(message = "Key is mandatory")
+    val key: String,
     val value: T?
 ) {
     constructor() : this("", null)
-
-    inline fun <reified T> getValueAs(): T? {
-        return value?.let { ObjectMapper().readValue(it.toString(), T::class.java) }
-    }
 }
