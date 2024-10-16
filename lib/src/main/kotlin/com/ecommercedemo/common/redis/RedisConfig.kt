3eb934ca8becc7e2aee15212b8d40096469f5ed6
aron <vaupelaron@gmail.com>
2024-10-16 18:30:02 +0700

    Add environment variables to RedisConfig

--- lib/src/main/kotlin/com/ecommercedemo/common/redis/RedisConfig.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/redis/RedisConfig.kt
@@ -1,5 +1,6 @@
 package com.ecommercedemo.common.redis
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
@@ -11,9 +12,16 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 @Configuration
 open class RedisConfig {
 
+    @Value("\${spring.data.redis.host}")
+    private lateinit var redisHost: String
+
+    @Value("\${spring.data.redis.port}")
+    private var redisPort: Int = 0
+
     @Bean
     open fun redisConnectionFactory(): RedisConnectionFactory {
-        return LettuceConnectionFactory()
+        val lettuceConnectionFactory = LettuceConnectionFactory(redisHost, redisPort)
+        return lettuceConnectionFactory
     }
 
     @Bean
