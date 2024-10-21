edfb2a75c18a19b8782e51bc853fe58e5de60b94
aron <vaupelaron@gmail.com>
2024-10-21 17:32:50 +0700

    Make EntityScanner and ApplicationStartup conditional on bean level

--- lib/src/main/kotlin/com/ecommercedemo/common/util/springboot/EntityScanner.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/util/springboot/EntityScanner.kt
@@ -2,11 +2,11 @@ package com.ecommercedemo.common.util.springboot
 
 import jakarta.persistence.EntityManagerFactory
 import jakarta.persistence.PersistenceUnit
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.stereotype.Component
 
 @Component
-@ConditionalOnClass(EntityManagerFactory::class)
+@ConditionalOnBean(EntityManagerFactory::class)
 class EntityScanner(
     @PersistenceUnit private val entityManagerFactory: EntityManagerFactory,
 ) {
