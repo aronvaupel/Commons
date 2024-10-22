38a9c2a5dcbb6bd25c7a092c2bede45dc4d02d34
aron <vaupelaron@gmail.com>
2024-10-22 20:57:30 +0700

    Change conditions on beans

--- lib/src/main/kotlin/com/ecommercedemo/common/util/springboot/ApplicationStartUp.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/util/springboot/ApplicationStartUp.kt
@@ -3,11 +3,9 @@ package com.ecommercedemo.common.util.springboot
 import com.ecommercedemo.common.kafka.DynamicTopicRegistration
 import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
 import org.springframework.stereotype.Component
 
 @Component
-@ConditionalOnExpression("'\${spring.kafka.bootstrap-servers:}' != ''")
 class ApplicationStartup(
     @Autowired private val dynamicTopicRegistration: DynamicTopicRegistration,
     @Autowired private val entityScanner: EntityScanner,
