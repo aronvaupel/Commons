e64764b16e873872e77f97f14a74d420bcdbad40
aron <vaupelaron@gmail.com>
2024-10-23 20:23:02 +0700

    Remove @DependsOn from EntityScanner bc it prevents @ConditionalOnClass to take Effect

--- lib/src/main/kotlin/com/ecommercedemo/common/util/springboot/EntityScanner.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/util/springboot/EntityScanner.kt
@@ -3,18 +3,13 @@ package com.ecommercedemo.common.util.springboot
 import jakarta.persistence.EntityManagerFactory
 import jakarta.persistence.PersistenceUnit
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
-import org.springframework.context.annotation.DependsOn
-import org.springframework.context.annotation.Lazy
 import org.springframework.stereotype.Component
 
 @Component
-@Lazy
 @ConditionalOnClass(name = ["jakarta.persistence.EntityManagerFactory"])
-@DependsOn("entityManagerFactory")
 class EntityScanner(
     @PersistenceUnit private val entityManagerFactory: EntityManagerFactory,
 ) {
-
     private fun getEntityNames(filterCondition: (String) -> Boolean): List<String> {
         val entityManager = entityManagerFactory.createEntityManager()
         val metamodel = entityManager.metamodel
