b2185d397bc1de6b2ffb0b69cd733a6f30bf0fcb
aron <vaupelaron@gmail.com>
2024-11-13 18:31:44 +0700

    fix params

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "2.2.2"
+version = "2.2.3"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/util/search/Retriever.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/util/search/Retriever.kt
@@ -32,7 +32,7 @@ class Retriever(
             } else {
                 val expectedValueType = resolvedPathInfo.jpaPath.model.bindableJavaType
                 val actualValue = deserializer.convertAnyIfNeeded(param.searchValue, expectedValueType)
-                validator.validate(param.searchValue, expectedValueType, param.path, entity, resolvedPathInfo.jpaPath.toString())
+                validator.validate(actualValue, expectedValueType, param.path, entity, resolvedPathInfo.jpaPath.toString())
                 param.operator.buildPredicate(criteriaBuilder, resolvedPathInfo.jpaPath, actualValue)
             }
         }
