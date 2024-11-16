2966e6e62149b629a18207392410890a18f7d62e
aron <vaupelaron@gmail.com>
2024-11-16 19:09:31 +0700

    Remove obsolete interface

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "2.4.0"
+version = "2.4.6"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/persistence/IPseudoPropertyManagement.kt
+++
@@ -1,6 +0,0 @@
-package com.ecommercedemo.common.persistence
-
-interface IPseudoPropertyManagement {
-    fun updatePseudoPropertyForAllEntities(key: String, value: Any?)
-    fun deletePseudoPropertyForAllEntities(key: String)
-}
