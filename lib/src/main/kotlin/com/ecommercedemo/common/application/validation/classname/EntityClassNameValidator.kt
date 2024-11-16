7071032559180bfbd18d7fbf7135c2e7e2547b16
aron <vaupelaron@gmail.com>
2024-11-16 20:19:31 +0700

    trial and error

--- lib/src/main/kotlin/com/ecommercedemo/common/application/validation/classname/EntityClassNameValidator.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/validation/classname/EntityClassNameValidator.kt
@@ -3,12 +3,15 @@ package com.ecommercedemo.common.application.validation.classname
 import jakarta.persistence.EntityManager
 import jakarta.validation.ConstraintValidator
 import jakarta.validation.ConstraintValidatorContext
+import org.hibernate.validator.internal.util.stereotypes.Lazy
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class EntityClassNameValidator(
-    private var entityManager: EntityManager
-): ConstraintValidator<ValidEntityClassName, String> {
+class EntityClassNameValidator: ConstraintValidator<ValidEntityClassName, String> {
+    @Autowired
+    @Lazy
+    private lateinit var entityManager: EntityManager
 
     override fun isValid(value: String, context: ConstraintValidatorContext?): Boolean {
         return try {
