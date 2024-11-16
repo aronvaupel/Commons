18c208fe358cb61ccba814ec96115a5668e0bbf6
aron <vaupelaron@gmail.com>
2024-11-16 15:54:41 +0700

    Make PseudoProperty an  entity

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "2.3.3"
+version = "2.3.4"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/model/PseudoProperty.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/PseudoProperty.kt
@@ -3,26 +3,31 @@ package com.ecommercedemo.common.model
 import com.ecommercedemo.common.model.dto.PseudoPropertyDto
 import com.ecommercedemo.common.util.JsonbConverter
 import com.ecommercedemo.common.validation.classname.ValidEntityClassName
-import jakarta.persistence.Column
-import jakarta.persistence.Convert
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
+import jakarta.persistence.*
+import jakarta.validation.constraints.NotNull
 import java.util.*
 import kotlin.reflect.KClass
 
 
 @Suppress("unused")
+@Entity
+@Table(name = PseudoProperty.STORAGE_NAME)
 open class PseudoProperty(
     @Id
     @GeneratedValue(generator = "uuid")
     open val id: UUID = UUID.randomUUID(),
     @ValidEntityClassName
     val entityClassName: String = Any::class.qualifiedName!!,
+    @NotNull
     var key: String = "",
     @Convert(converter = JsonbConverter::class)
     @Column(columnDefinition = "jsonb")
     var valueType: Any
 ) {
+    constructor() : this(UUID.randomUUID(), Any::class.qualifiedName!!, "", "") {
+
+    }
+
     private val entity: KClass<*>
         get() = Class.forName(entityClassName).kotlin
 
@@ -37,6 +42,10 @@ open class PseudoProperty(
     ) = PseudoProperty(id, entityClassName, key, value)
 
     fun toDto() = PseudoPropertyDto(entityClassName, key, valueType)
+
+    companion object {
+        const val STORAGE_NAME = "pseudo_properties"
+    }
 }
 
 
