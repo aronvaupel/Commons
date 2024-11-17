080ef557937de7d65cfc66138bef2840522fc939
aron <vaupelaron@gmail.com>
2024-11-17 16:50:22 +0700

    Fix repo

--- lib/src/main/kotlin/com/ecommercedemo/common/persistence/PseudoPropertyRepository.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/persistence/PseudoPropertyRepository.kt
@@ -1,10 +1,26 @@
 package com.ecommercedemo.common.persistence
 
 import com.ecommercedemo.common.model.PseudoProperty
-import com.ecommercedemo.common.model.dto.PseudoPropertyDto
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import java.time.LocalDateTime
 import java.util.*
 
 interface PseudoPropertyRepository: JpaRepository<PseudoProperty, UUID> {
-    fun save(pseudoProperty: PseudoPropertyDto): PseudoProperty
+    @Modifying
+    @Query(
+        value = "INSERT INTO pseudo_properties (created_at, entity_simple_name, key, type_descriptor, updated_at, id) " +
+                "VALUES (:createdAt, :entitySimpleName, :key, CAST(:typeDescriptor AS JSONB), :updatedAt, :id)",
+        nativeQuery = true
+    )
+    fun save(
+        @Param("createdAt") createdAt: LocalDateTime,
+        @Param("entitySimpleName") entitySimpleName: String,
+        @Param("key") key: String,
+        @Param("typeDescriptor") typeDescriptor: String,
+        @Param("updatedAt") updatedAt: LocalDateTime,
+        @Param("id") id: UUID
+    )
 }
