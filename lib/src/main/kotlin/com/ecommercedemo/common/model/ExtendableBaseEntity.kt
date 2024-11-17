943317c9f16f222d9ed23c20e1218b0d18905301
aron <vaupelaron@gmail.com>
2024-11-17 20:53:28 +0700

    add getters and initialize with default

--- lib/src/main/kotlin/com/ecommercedemo/common/model/ExtendableBaseEntity.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/ExtendableBaseEntity.kt
@@ -1,5 +1,7 @@
 package com.ecommercedemo.common.model
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.hypersistence.utils.hibernate.type.json.JsonBinaryType
 import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
@@ -11,5 +13,16 @@ import org.hibernate.annotations.Type
 abstract class ExtendableBaseEntity: BaseEntity() {
     @Type(JsonBinaryType::class)
     @Column(name = "pseudo_property", columnDefinition = "jsonb")
-    open lateinit var pseudoProperties: String
+    open var pseudoProperties: String = "{}"
+
+    fun getPseudoProperty(key: String): Any? {
+        val map: Map<String, Any?> = ObjectMapper().readValue(pseudoProperties, object : TypeReference<Map<String, Any?>>() {})
+        return map[key]
+    }
+
+    fun setPseudoProperty(key: String, value: Any) {
+        val map: MutableMap<String, Any?> = ObjectMapper().readValue(pseudoProperties, object : TypeReference<MutableMap<String, Any?>>() {})
+        map[key] = value
+        pseudoProperties = ObjectMapper().writeValueAsString(map)
+    }
 }
