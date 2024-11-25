98e0afbe1ebae76e100d00c657ded6ae0dd1a545
aron <vaupelaron@gmail.com>
2024-11-25 15:50:25 +0700

    Try default constructor

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.0.6"
+version = "3.0.7"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/RestControllerTemplate.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/RestControllerTemplate.kt
@@ -5,7 +5,6 @@ import com.ecommercedemo.common.controller.abstraction.request.SearchRequest
 import com.ecommercedemo.common.controller.abstraction.request.UpdateRequest
 import com.ecommercedemo.common.model.abstraction.BaseEntity
 import com.ecommercedemo.common.service.abstraction.ServiceTemplate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,8 +14,9 @@ import java.util.*
 abstract class RestControllerTemplate<T : BaseEntity> {
     private lateinit var service: ServiceTemplate<T>
 
-    @Autowired
-    fun setService(service: ServiceTemplate<T>) {
+    constructor()
+
+    constructor(service: ServiceTemplate<T>) {
         this.service = service
     }
 
