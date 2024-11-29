213afc8545fbca2e3e6c81582cd85b39d849f4d0
aron <vaupelaron@gmail.com>
2024-11-30 04:11:21 +0700

    attempt to fix time deserialization

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.1.38"
+version = "3.1.39"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/BaseEntity.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/BaseEntity.kt
@@ -1,5 +1,9 @@
 package com.ecommercedemo.common.model.abstraction
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
 import jakarta.persistence.*
 import java.time.LocalDateTime
 import java.util.*
@@ -13,9 +17,14 @@ abstract class BaseEntity{
     open val id: UUID = UUID.randomUUID()
 
     @Column(nullable = false, updatable = false)
+
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
     open var createdAt: LocalDateTime = LocalDateTime.now()
 
     @Column(nullable = false)
+    @JsonSerialize(using = LocalDateTimeSerializer::class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer::class)
     open var updatedAt: LocalDateTime = LocalDateTime.now()
 
     @PrePersist
