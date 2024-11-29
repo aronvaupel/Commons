4b8953a9e1a5db98cd6ff75c2f09054e636af4bb
aron <vaupelaron@gmail.com>
2024-11-30 02:21:53 +0700

    disable timestamps

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.1.32"
+version = "3.1.33"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/application/ObjectMapperConfig.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/ObjectMapperConfig.kt
@@ -1,6 +1,7 @@
 package com.ecommercedemo.common.application
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
@@ -14,6 +15,7 @@ open class ObjectMapperConfig {
         return ObjectMapper().apply {
             registerModule(KotlinModule.Builder().build())
             registerModule(JavaTimeModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
         }
     }
 }
