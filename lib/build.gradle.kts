3511fd30dce1459caa39c3e79e64c03ee4694cb5
aron <vaupelaron@gmail.com>
2024-11-30 01:40:11 +0700

    add ObjectMapperConfig

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.1.28"
+version = "3.1.29"
 
 repositories {
     mavenCentral()
---
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/ObjectMapperConfig.kt
@@ -0,0 +1,19 @@
+package com.ecommercedemo.common.application
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class ObjectMapperConfig {
+
+    @Bean
+    open fun objectMapper(): ObjectMapper {
+        return ObjectMapper().apply {
+            registerModule(KotlinModule.Builder().build())
+            registerModule(JavaTimeModule())
+        }
+    }
+}
