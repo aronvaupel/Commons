9691ab6f82fad8f40d46b7c0051025874562bcc1
aron <vaupelaron@gmail.com>
2024-12-01 13:59:40 +0700

    try to preserve type info

--- lib/src/main/kotlin/com/ecommercedemo/common/application/event/EntityEvent.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/event/EntityEvent.kt
@@ -1,6 +1,7 @@
 package com.ecommercedemo.common.application.event
 
 import com.ecommercedemo.common.model.abstraction.BaseEntity
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.util.*
 
 
@@ -8,5 +9,6 @@ data class EntityEvent<T: BaseEntity>(
     val entityClass: Class<T>,
     val id: UUID,
     val type: EntityEventType,
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
     val properties: MutableMap<String, Any?>,
 )
