9fa9fbad41a354e67716ca2c046c2983bf0ffcd8
aron <vaupelaron@gmail.com>
2024-12-02 18:32:51 +0700

    Add subtyping again

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.4.9"
+version = "3.4.10"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/TypeDescriptor.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/TypeDescriptor.kt
@@ -3,8 +3,22 @@ package com.ecommercedemo.common.controller.abstraction.util
 
 import com.ecommercedemo.common.application.validation.type.TypeCategory
 import com.ecommercedemo.common.application.validation.type.ValueType
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import com.fasterxml.jackson.annotation.JsonTypeName
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "category" // Use the "category" field for type discrimination
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = TypeDescriptor.PrimitiveDescriptor::class, name = "PRIMITIVE"),
+    JsonSubTypes.Type(value = TypeDescriptor.TimeDescriptor::class, name = "TIME"),
+    JsonSubTypes.Type(value = TypeDescriptor.CollectionDescriptor::class, name = "COLLECTION"),
+    JsonSubTypes.Type(value = TypeDescriptor.MapDescriptor::class, name = "MAP"),
+    JsonSubTypes.Type(value = TypeDescriptor.ComplexObjectDescriptor::class, name = "COMPLEX")
+)
 sealed class TypeDescriptor {
     abstract val category: TypeCategory
     abstract val type: ValueType
