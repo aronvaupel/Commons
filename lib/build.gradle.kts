ff22011e7af2715cb4fcaa855bac05e4856fee79
aron <vaupelaron@gmail.com>
2024-12-09 23:52:29 +0700

    cast explicitly

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.5.31"
+version = "3.5.32"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/persistence/abstraction/PostgresEntityPersistenceAdapter.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/persistence/abstraction/PostgresEntityPersistenceAdapter.kt
@@ -7,7 +7,7 @@ abstract class PostgresEntityPersistenceAdapter<T : BaseEntity>(
     private val repository: EntityRepository<T, UUID>
 ): IEntityPersistenceAdapter<T> {
     override fun save(entity: T): T {
-        return repository.save(entity)
+        return repository.save(entity) as T
     }
 
     override fun delete(id: UUID) {
@@ -15,10 +15,10 @@ abstract class PostgresEntityPersistenceAdapter<T : BaseEntity>(
     }
 
     override fun getById(id: UUID): T {
-        return repository.findById(id).orElseThrow()
+        return repository.findById(id).orElseThrow() as T
     }
 
     override fun getAllByIds(ids: List<UUID>): List<T> {
-        return repository.findAllById(ids)
+        return repository.findAllById(ids) as List<T>
     }
 }
