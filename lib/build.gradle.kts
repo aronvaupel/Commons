1811aa7ac7de70343961de1808ff3be7356e4129
aron <vaupelaron@gmail.com>
2024-12-10 02:44:21 +0700

    Switch to JsonTypeInfo

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "3.6.4"
+version = "3.6.5"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/application/event/EntityEvent.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/event/EntityEvent.kt
@@ -1,6 +1,6 @@
 package com.ecommercedemo.common.application.event
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import java.util.*
 
 
@@ -8,7 +8,7 @@ data class EntityEvent(
     val entityClassName: String,
     val id: UUID,
     val type: EntityEventType,
-    //@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
-    @JsonDeserialize(using = PropertiesDeserializer::class)
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
+    //@JsonDeserialize(using = PropertiesDeserializer::class)
     val properties: MutableMap<String, Any?>,
 )
