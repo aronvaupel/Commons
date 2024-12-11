2e26ea49375aaa1fcbeaf9f49b6c1aab92acf57d
aron <vaupelaron@gmail.com>
2024-12-11 21:08:02 +0700

    attempt to improve desrialization

--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/request/CreateRequest.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/request/CreateRequest.kt
@@ -1,6 +1,9 @@
 package com.ecommercedemo.common.controller.abstraction.request
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+
 data class CreateRequest(
     val entityClassName: String,
+    @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY, property = "@class")
     val properties: Map<String, Any?> = emptyMap()
 )
