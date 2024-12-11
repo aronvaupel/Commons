8e2214160a6c0f6708f817c13e16e1ce0a61887c
aron <vaupelaron@gmail.com>
2024-12-12 00:46:39 +0700

    simplify

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "4.0.9test11"
+version = "4.0.9test12"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/ServiceUtility.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/ServiceUtility.kt
@@ -34,6 +34,10 @@ class ServiceUtility(
             targetPropertyMap.values
                 .firstOrNull { it.name == key || it.name.removePrefix("_") == key }?.name
                 ?: throw IllegalArgumentException("Field $key does not exist in the entity.")
+        }.let { mappedProperties ->
+            objectMapper.readValue(objectMapper.writeValueAsString(mappedProperties), instanceClass.java)
+        }::class.memberProperties.associateBy { it.name }.mapValues { (name, property) ->
+            properties[name.removePrefix("_")] ?: property.getter.call()
         }
         println("resolvedProperties: $resolvedProperties")
 
