c17178380cb18a9970ae05edc19f41ddfa170679
aron <vaupelaron@gmail.com>
2024-12-12 17:32:08 +0700

    remove sql-type annotation and use json binary again

--- lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/AugmentableBaseEntity.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/AugmentableBaseEntity.kt
@@ -1,18 +1,15 @@
 package com.ecommercedemo.common.model.abstraction
 
-import io.hypersistence.utils.hibernate.type.json.JsonType
+import io.hypersistence.utils.hibernate.type.json.JsonBinaryType
 import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
-import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.annotations.Type
-import org.hibernate.type.SqlTypes
 
 
 @MappedSuperclass
 @Suppress("unused")
 abstract class AugmentableBaseEntity: BaseEntity() {
-    @get:Type(JsonType::class)
-    @get:JdbcTypeCode(SqlTypes.JSON)
+    @get:Type(JsonBinaryType::class)
     @get:Column(name = "pseudo_properties", columnDefinition = "jsonb")
     open var pseudoProperties: MutableMap<String, Any?> = mutableMapOf()
 
--- lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/BasePseudoProperty.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/BasePseudoProperty.kt
@@ -2,12 +2,10 @@ package com.ecommercedemo.common.model.abstraction
 
 import com.ecommercedemo.common.application.validation.type.ValueType
 import com.ecommercedemo.common.controller.abstraction.util.TypeDescriptor
-import io.hypersistence.utils.hibernate.type.json.JsonType
+import io.hypersistence.utils.hibernate.type.json.JsonBinaryType
 import jakarta.persistence.Column
 import jakarta.persistence.MappedSuperclass
-import org.hibernate.annotations.JdbcTypeCode
 import org.hibernate.annotations.Type
-import org.hibernate.type.SqlTypes
 
 @MappedSuperclass
 abstract class BasePseudoProperty : BaseEntity() {
@@ -15,8 +13,7 @@ abstract class BasePseudoProperty : BaseEntity() {
     open val entitySimpleName: String = ""
     open var key: String = ""
 
-    @get:Type(JsonType::class)
-    @get:JdbcTypeCode(SqlTypes.JSON)
+    @get:Type(JsonBinaryType::class)
     @get:Column(name = "type_descriptor", columnDefinition = "jsonb")
     open var typeDescriptor: TypeDescriptor = TypeDescriptor.PrimitiveDescriptor(
         category = "PRIMITIVE",
