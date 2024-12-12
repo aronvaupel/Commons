e95559baabf26cd9ba6d24d8f4fdc5199693c757
aron <vaupelaron@gmail.com>
2024-12-12 21:02:51 +0700

    rm @get

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "4.1.16"
+version = "4.1.17"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/AugmentableBaseEntity.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/AugmentableBaseEntity.kt
@@ -7,47 +7,59 @@ import org.hibernate.annotations.Type
 
 
 @MappedSuperclass
-@Suppress("unused")
+@Suppress("unused", "UNCHECKED_CAST")
 abstract class AugmentableBaseEntity: BaseEntity() {
-    @get:Type(JsonType::class)
-    @get:Column(name = "pseudo_properties", columnDefinition = "jsonb")
-    open var pseudoProperties: Map<String, Any?> = mapOf()
+    @Type(JsonType::class)
+    @Column(name = "pseudo_properties", columnDefinition = "jsonb")
+    open var pseudoPropertiesJson: Map<String, Any?> = mapOf()
 
-    fun getPseudoProperty(key: String): Any? {
-        return pseudoProperties[key]
-    }
+//    @Transient
+//    var pseudoProperties: Map<String, Any?> = emptyMap()
+//        get() = objectMapper.readValue(pseudoPropertiesJson, object : TypeReference<Map<String, Any?>>() {})
+//        set(value) {
+//            field = value
+//            pseudoPropertiesJson = objectMapper.writeValueAsString(value)
+//        }
+//
+//    companion object {
+//        private val objectMapper = ObjectMapper()
+//    }
 
-    fun addPseudoProperty(key: String, value: Any?) {
-        pseudoProperties = pseudoProperties.toMutableMap().apply {
-            this[key] = value
-        }
-    }
-
-    fun removePseudoProperty(key: String) {
-        pseudoProperties = pseudoProperties.toMutableMap().apply {
-            this.remove(key)
-        }
-    }
-
-    fun renamePseudoProperty(oldKey: String, newKey: String) {
-        try {
-            require(oldKey.isNotBlank()) { "Old key must not be blank." }
-            require(newKey.isNotBlank()) { "New key must not be blank." }
-            if (oldKey == newKey) {
-                println("No renaming needed: 'oldKey' and 'newKey' are the same.")
-                return
-            }
-            if (!pseudoProperties.containsKey(oldKey)) {
-                throw IllegalArgumentException("Key '$oldKey' does not exist in pseudoProperties.")
-            }
-
-            pseudoProperties = pseudoProperties.toMutableMap().apply {
-                this[newKey] = this.remove(oldKey)
-            }
-            println("Successfully renamed key '$oldKey' to '$newKey'.")
-        } catch (e: Exception) {
-            println("Failed to rename key: ${e.message}")
-            throw e
-        }
-    }
+//    fun getPseudoProperty(key: String): Any? {
+//        return pseudoProperties[key]
+//    }
+//
+//    fun addPseudoProperty(key: String, value: Any?) {
+//        pseudoProperties = pseudoProperties.toMutableMap().apply {
+//            this[key] = value
+//        }
+//    }
+//
+//    fun removePseudoProperty(key: String) {
+//        pseudoProperties = pseudoProperties.toMutableMap().apply {
+//            this.remove(key)
+//        }
+//    }
+//
+//    fun renamePseudoProperty(oldKey: String, newKey: String) {
+//        try {
+//            require(oldKey.isNotBlank()) { "Old key must not be blank." }
+//            require(newKey.isNotBlank()) { "New key must not be blank." }
+//            if (oldKey == newKey) {
+//                println("No renaming needed: 'oldKey' and 'newKey' are the same.")
+//                return
+//            }
+//            if (!pseudoProperties.containsKey(oldKey)) {
+//                throw IllegalArgumentException("Key '$oldKey' does not exist in pseudoProperties.")
+//            }
+//
+//            pseudoProperties = pseudoProperties.toMutableMap().apply {
+//                this[newKey] = this.remove(oldKey)
+//            }
+//            println("Successfully renamed key '$oldKey' to '$newKey'.")
+//        } catch (e: Exception) {
+//            println("Failed to rename key: ${e.message}")
+//            throw e
+//        }
+//    }
 }
