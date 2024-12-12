d23234d41e1c2ae79a95d41d39f012009d813286
aron <vaupelaron@gmail.com>
2024-12-12 21:04:25 +0700

    comment in

--- lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/AugmentableBaseEntity.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/model/abstraction/AugmentableBaseEntity.kt
@@ -25,41 +25,41 @@ abstract class AugmentableBaseEntity: BaseEntity() {
 //        private val objectMapper = ObjectMapper()
 //    }
 
-//    fun getPseudoProperty(key: String): Any? {
-//        return pseudoProperties[key]
-//    }
-//
-//    fun addPseudoProperty(key: String, value: Any?) {
-//        pseudoProperties = pseudoProperties.toMutableMap().apply {
-//            this[key] = value
-//        }
-//    }
-//
-//    fun removePseudoProperty(key: String) {
-//        pseudoProperties = pseudoProperties.toMutableMap().apply {
-//            this.remove(key)
-//        }
-//    }
-//
-//    fun renamePseudoProperty(oldKey: String, newKey: String) {
-//        try {
-//            require(oldKey.isNotBlank()) { "Old key must not be blank." }
-//            require(newKey.isNotBlank()) { "New key must not be blank." }
-//            if (oldKey == newKey) {
-//                println("No renaming needed: 'oldKey' and 'newKey' are the same.")
-//                return
-//            }
-//            if (!pseudoProperties.containsKey(oldKey)) {
-//                throw IllegalArgumentException("Key '$oldKey' does not exist in pseudoProperties.")
-//            }
-//
-//            pseudoProperties = pseudoProperties.toMutableMap().apply {
-//                this[newKey] = this.remove(oldKey)
-//            }
-//            println("Successfully renamed key '$oldKey' to '$newKey'.")
-//        } catch (e: Exception) {
-//            println("Failed to rename key: ${e.message}")
-//            throw e
-//        }
-//    }
+    fun getPseudoProperty(key: String): Any? {
+        return pseudoProperties[key]
+    }
+
+    fun addPseudoProperty(key: String, value: Any?) {
+        pseudoProperties = pseudoProperties.toMutableMap().apply {
+            this[key] = value
+        }
+    }
+
+    fun removePseudoProperty(key: String) {
+        pseudoProperties = pseudoProperties.toMutableMap().apply {
+            this.remove(key)
+        }
+    }
+
+    fun renamePseudoProperty(oldKey: String, newKey: String) {
+        try {
+            require(oldKey.isNotBlank()) { "Old key must not be blank." }
+            require(newKey.isNotBlank()) { "New key must not be blank." }
+            if (oldKey == newKey) {
+                println("No renaming needed: 'oldKey' and 'newKey' are the same.")
+                return
+            }
+            if (!pseudoProperties.containsKey(oldKey)) {
+                throw IllegalArgumentException("Key '$oldKey' does not exist in pseudoProperties.")
+            }
+
+            pseudoProperties = pseudoProperties.toMutableMap().apply {
+                this[newKey] = this.remove(oldKey)
+            }
+            println("Successfully renamed key '$oldKey' to '$newKey'.")
+        } catch (e: Exception) {
+            println("Failed to rename key: ${e.message}")
+            throw e
+        }
+    }
 }
