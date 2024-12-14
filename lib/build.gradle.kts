c6c1c7ce6ca75ffb18f23d880c5d9b543ee54509
aron <vaupelaron@gmail.com>
2024-12-14 17:22:21 +0700

    debugging, no msg

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "4.3.38"
+version = "4.3.39"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/TypeReAttacher.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/TypeReAttacher.kt
@@ -1,9 +1,12 @@
 package com.ecommercedemo.common.service.concretion
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.stereotype.Service
 import kotlin.reflect.KClass
+import kotlin.reflect.KType
 import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.javaType
 
 @Service
 @Suppress("UNCHECKED_CAST")
@@ -49,12 +52,12 @@ class TypeReAttacher(
 //        return typedData
 //    }
 
-    private fun extractFieldTypesMap(entityClass: KClass<*>): Map<String, Class<*>> {
-        return entityClass.memberProperties.associate { property ->
-            val kClass = property.returnType.classifier as? KClass<*>
-                ?: throw IllegalArgumentException("Cannot extract KClass for property: ${property.name}")
-            property.name to kClass.java
+    private fun extractFieldTypesMap(entityClass: KClass<*>): Map<String, KType> {
+        val result =  entityClass.memberProperties.associate { property ->
+            property.name to property.returnType
         }
+        println("FIELD TYPES: $result")
+        return result
     }
 
 
@@ -66,9 +69,11 @@ class TypeReAttacher(
         val  typesForDataKeys = extractFieldTypesMap(entityClass).filterKeys { data.containsKey(it) }
         println("TYPES FOR DATA KEYS: $typesForDataKeys")
 
-        val typedData: Map<String, Any?> = typesForDataKeys.mapValues { (key, targetClass) ->
-            val rawValue = data[key]
-            if (rawValue != null) objectMapper.convertValue(rawValue, targetClass)
+        val typedData: Map<String, Any?> = typesForDataKeys.mapValues { (key, kType) ->
+            val typeReference = object : TypeReference<Any>() {
+                override fun getType() = kType.javaType
+            }
+            objectMapper.convertValue(data[key], typeReference)
         }
 
         println("TYPED DATA: $typedData")
