579eb3b0e6ed00ba5b6dbe88650c80ff088e39e7
aron <vaupelaron@gmail.com>
2024-12-15 03:49:55 +0700

    debugging, no msg

--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/SearchParams.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/SearchParams.kt
@@ -3,6 +3,6 @@ package com.ecommercedemo.common.controller.abstraction.util
 @com.ecommercedemo.common.application.validation.operator.ValidOperator
 data class SearchParams(
     val operator: Operator,
-    val searchValue: Any?,
+    val searchValue: Any? = null,
     val path: String // "username", "userInfo.address.city", "userInfo.pseudoProperties.occupation.company"
 )
