2aa211816a931b2a9f1d180cce1139baf93a8c7e
aron <vaupelaron@gmail.com>
2024-12-15 19:02:42 +0700

    debugging, no msg

--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/PathResolver.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/PathResolver.kt
@@ -2,6 +2,7 @@ package com.ecommercedemo.common.controller.abstraction.util
 
 import com.ecommercedemo.common.model.abstraction.AugmentableBaseEntity
 import com.ecommercedemo.common.model.abstraction.BaseEntity
+import com.ecommercedemo.common.persistence.concretion._pseudoProperty._PseudoPropertyRepository
 import jakarta.persistence.criteria.Path
 import jakarta.persistence.criteria.Root
 import org.springframework.stereotype.Service
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Service
 class PathResolver(
     private val validator: SearchParamValidation,
     private val deserializer: SearchParamDeserializer,
-
+    private val _pseudoPropertyRepository: _PseudoPropertyRepository,
     ) {
     fun <T : BaseEntity> resolvePath(params: SearchParams, root: Root<T>): ResolvedSearchParam {
         println("Resolving path: ${params.path}")
@@ -22,17 +23,29 @@ class PathResolver(
         var currentClass: Class<*> = root.javaType
         println("CURRENT CLASS: $currentClass")
 
+        val registeredPseudoPropertyTypesMap = _pseudoPropertyRepository.findAllByEntitySimpleName(currentClass.simpleName)
+            .associate { it.key to it.typeDescriptor.type.typeInfo }
+
         segments.forEachIndexed { index, segment ->
             println("SEGMENT: $segment")
             validator.validateFieldExistsAndIsAccessible(segment, currentClass)
             if (segment == AugmentableBaseEntity::pseudoProperties.name) {
                 val jsonSegments = segments.drop(index + 1)
                 println("JSON SEGMENTS: $jsonSegments")
-                currentClass = currentPath.get<Any>(segment).model.bindableJavaType
-                println("CURRENT CLASS FOR SEGMENT: $currentClass")
-                val actualValue = deserializer.convertAnyIfNeeded(params.searchValue, currentClass)
+                val actualValue =  jsonSegments.map { jsonSegment ->
+                   println("JSON SEGMENT: $jsonSegment")
+                    val segmentValue = deserializer.convertAnyIfNeeded(
+                        params.searchValue,
+                        registeredPseudoPropertyTypesMap[jsonSegment]
+                            ?: throw IllegalArgumentException("PseudoProperty type not found")
+                    )
+                    println("JSON SEGMENT VALUE: $segmentValue")
+                }
                 println("ACTUAL VALUE: $actualValue")
-                return ResolvedSearchParam(actualValue, jpaPath = currentPath.get<Any>(segment), jsonSegments = jsonSegments)
+                return ResolvedSearchParam(
+                    deserializedValue = actualValue,
+                    jpaPath = currentPath.get<Any>(segment),
+                    jsonSegments = jsonSegments)
             } else {
                 currentPath = currentPath.get<Any>(segment)
                 currentClass = currentPath.model.bindableJavaType
