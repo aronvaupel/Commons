7adf37438505299cef1b3ce71558ccee5ce9a1b6
aron <vaupelaron@gmail.com>
2024-12-17 23:55:25 +0700

    Debugging, no msg

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "4.6.1"
+version = "4.6.3"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/service/TemplateBeanRegistrar.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/TemplateBeanRegistrar.kt
@@ -1,6 +1,7 @@
 package com.ecommercedemo.common.service
 
 import com.ecommercedemo.common.application.ClassPathScanner
+import com.ecommercedemo.common.application.SpringContextProvider
 import com.ecommercedemo.common.controller.ControllerFor
 import com.ecommercedemo.common.persistence.PersistenceAdapterFor
 import org.springframework.beans.factory.support.BeanDefinitionBuilder
@@ -9,15 +10,27 @@ import org.springframework.context.annotation.ImportBeanDefinitionRegistrar
 import org.springframework.core.type.AnnotationMetadata
 
 class TemplateBeanRegistrar : ImportBeanDefinitionRegistrar {
-   // private val applicationContext = SpringContextProvider.applicationContext
 
     override fun registerBeanDefinitions(metadata: AnnotationMetadata, registry: BeanDefinitionRegistry) {
         val scanner = ClassPathScanner()
 
         scanner.findClassesWithAnnotation(RestServiceFor::class).forEach { clazz ->
-            val beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(clazz).beanDefinition
-            registry.registerBeanDefinition(clazz.simpleName, beanDefinition)
+            val entityClass = clazz.getAnnotation(RestServiceFor::class.java).entity.java
+            val parentConstructor = clazz.superclass.constructors.firstOrNull()
+                ?: throw IllegalStateException("No constructor found for parent class of ${clazz.simpleName}")
+
+            val dependencies = parentConstructor.parameterTypes.map { paramType ->
+                when (paramType) {
+                    entityClass::class.java -> entityClass
+                    else -> SpringContextProvider.applicationContext.getBean(paramType)
+                }
+            }
+
+            val beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(clazz)
+            dependencies.forEach { dep -> beanDefinition.addConstructorArgValue(dep) }
+            registry.registerBeanDefinition(clazz.simpleName, beanDefinition.beanDefinition)
         }
+
         scanner.findClassesWithAnnotation(EventServiceFor::class).forEach { clazz ->
             val beanDefinition = BeanDefinitionBuilder.genericBeanDefinition(clazz).beanDefinition
             registry.registerBeanDefinition(clazz.simpleName, beanDefinition)
@@ -31,35 +44,5 @@ class TemplateBeanRegistrar : ImportBeanDefinitionRegistrar {
             registry.registerBeanDefinition(clazz.simpleName, beanDefinition)
         }
     }
-
-//    private fun registerBean(clazz: Class<*>, registry: BeanDefinitionRegistry, context: ApplicationContext) {
-//        val kClass = clazz.kotlin
-//
-//        val annotation = kClass.findAnnotation<RestServiceFor>()
-//            ?: throw IllegalStateException("No @RestServiceFor annotation found on ${clazz.simpleName}")
-//
-//        val entityClass = annotation.entity.java.kotlin
-//
-//        val parentClass = kClass.superclasses.firstOrNull()
-//            ?: throw IllegalStateException("No superclass found for ${clazz.simpleName}")
-//
-//        val parentConstructor = parentClass.primaryConstructor
-//            ?: throw IllegalStateException("No primary constructor found for ${parentClass.simpleName}")
-//
-//        val beanDefinitionBuilder = BeanDefinitionBuilder.genericBeanDefinition(clazz)
-//
-//        parentConstructor.parameters.forEach { param ->
-//            val paramClass = param.type.classifier?.javaClass
-//            val dependency = if (param.name == "entityClass") {
-//                entityClass
-//            } else {
-//                paramClass?.let { context.getBean(it) }
-//                    ?: throw IllegalStateException("No bean found for parameter ${param.name}")
-//            }
-//            beanDefinitionBuilder.addConstructorArgValue(dependency)
-//        }
-//
-//        registry.registerBeanDefinition(clazz.simpleName, beanDefinitionBuilder.beanDefinition)
-//    }
 
 }
