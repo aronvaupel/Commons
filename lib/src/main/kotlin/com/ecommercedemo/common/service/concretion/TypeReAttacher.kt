aa8008052791c5d71865fa76b878a6038c024d93
aron <vaupelaron@gmail.com>
2024-12-25 22:52:52 +0700

    debugging, no msg

--- lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/TypeReAttacher.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/TypeReAttacher.kt
@@ -8,21 +8,21 @@ import kotlin.reflect.KClass
 import kotlin.reflect.jvm.javaType
 
 @Service
-class TypeReAttacher(
-    private val reflectionService: ReflectionService,
-) {
+class TypeReAttacher {
 
     fun reAttachType(
         data: Map<String, Any?>,
         entityClass: KClass<*>
     ): Map<String, Any?> {
-        val typesForDataKeys = reflectionService.extractFieldTypesMap(entityClass).filterKeys { data.containsKey(it) }
+        val typesForDataKeys = SpringContextProvider.applicationContext.getBean(ReflectionService::class.java)
+            .extractFieldTypesMap(entityClass).filterKeys { data.containsKey(it) }
 
         val typedData: Map<String, Any?> = typesForDataKeys.mapValues { (key, kType) ->
             val typeReference = object : TypeReference<Any>() {
                 override fun getType() = kType.javaType
             }
-            SpringContextProvider.applicationContext.getBean(ObjectMapper::class.java).convertValue(data[key], typeReference)
+            SpringContextProvider.applicationContext.getBean(ObjectMapper::class.java)
+                .convertValue(data[key], typeReference)
         }
         return typedData
     }
