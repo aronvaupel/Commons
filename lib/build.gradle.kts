ece71ebf84d90288f18e70d64d7a3e1a0b221f5e
aron <vaupelaron@gmail.com>
2024-12-26 19:48:08 +0700

    debugging, no msg

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "6.1.28"
+version = "6.1.29"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/TypeReAttacher.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/TypeReAttacher.kt
@@ -1,11 +1,8 @@
 package com.ecommercedemo.common.service.concretion
 
 import com.ecommercedemo.common.application.SpringContextProvider
-import com.ecommercedemo.common.application.cache.RedisService
-import com.ecommercedemo.common.application.exception.NotCachedException
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import kotlin.reflect.KClass
 import kotlin.reflect.KType
@@ -13,56 +10,30 @@ import kotlin.reflect.full.memberProperties
 import kotlin.reflect.jvm.javaType
 
 @Service
+
 class TypeReAttacher {
-    @Autowired
-    private lateinit var redisService: RedisService
-
-    val log = mu.KotlinLogging.logger {}
     private fun extractFieldTypesMap(entityClass: KClass<*>): Map<String, KType> {
-        log.info { "Uses extractFieldTypesMap" }
-        val start = System.currentTimeMillis()
-        return try {
-            val fromCache = redisService.getCachedMethodResultOrThrow(
-                "extractFieldTypesMap",
-                listOf(entityClass),
-                object : TypeReference<Map<String, KType>>() {}
-            )
-            log.info { "Got from cache" }
-            val end = System.currentTimeMillis()
-            log.info { "Time elapsed: ${end - start} ms" }
-            fromCache
-        } catch (e: NotCachedException) {
-            val computed = entityClass.memberProperties.associate { property ->
-                property.name to property.returnType
-            }
-            redisService.cacheMethodResult(
-                "extractFieldTypesMap",
-                listOf(entityClass),
-                computed
-            )
-            log.info { "Saved to cache" }
-            val end = System.currentTimeMillis()
-            log.info { "Time elapsed: ${end - start} ms" }
-            computed
+        return entityClass.memberProperties.associate { property ->
+            property.name to property.returnType
         }
     }
 
-    fun reAttachType(
-        data: Map<String, Any?>,
-        entityClass: KClass<*>
-    ): Map<String, Any?> {
+        fun reAttachType(
+            data: Map<String, Any?>,
+            entityClass: KClass<*>
+        ): Map<String, Any?> {
 
 
-        val typesForDataKeys = extractFieldTypesMap(entityClass).filterKeys { data.containsKey(it) }
+            val typesForDataKeys = extractFieldTypesMap(entityClass).filterKeys { data.containsKey(it) }
 
-        val typedData: Map<String, Any?> = typesForDataKeys.mapValues { (key, kType) ->
-            val typeReference = object : TypeReference<Any>() {
-                override fun getType() = kType.javaType
+            val typedData: Map<String, Any?> = typesForDataKeys.mapValues { (key, kType) ->
+                val typeReference = object : TypeReference<Any>() {
+                    override fun getType() = kType.javaType
+                }
+                SpringContextProvider.applicationContext.getBean(ObjectMapper::class.java)
+                    .convertValue(data[key], typeReference)
             }
-            SpringContextProvider.applicationContext.getBean(ObjectMapper::class.java)
-                .convertValue(data[key], typeReference)
+            return typedData
         }
-        return typedData
-    }
 
-}
+    }
