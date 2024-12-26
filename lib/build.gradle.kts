fdf6431490099dcbc110442923867c9e9e2828e9
aron <vaupelaron@gmail.com>
2024-12-27 02:22:31 +0700

    debugging, no msg

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "6.2.6"
+version = "6.2.7"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/application/cache/SerializablePage.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/cache/SerializablePage.kt
@@ -3,9 +3,10 @@ package com.ecommercedemo.common.application.cache
 import com.ecommercedemo.common.model.abstraction.BaseEntity
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import kotlin.reflect.KClass
 
 
-data class SerializablePage<T: BaseEntity> @JsonCreator constructor(
+data class SerializablePage<T: KClass<out BaseEntity>> @JsonCreator constructor(
     @JsonProperty("content") val content: MutableList<T>,
     @JsonProperty("page")val page: Int,
     @JsonProperty("size") val size: Int,
