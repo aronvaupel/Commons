1e243bc1d8967656ed119c46e9b5589099dea9ac
aron <vaupelaron@gmail.com>
2024-12-26 07:38:09 +0700

    debugging, no msg

--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/Retriever.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/util/Retriever.kt
@@ -4,6 +4,7 @@ import com.ecommercedemo.common.application.cache.RedisService
 import com.ecommercedemo.common.controller.abstraction.request.SearchRequest
 import com.ecommercedemo.common.model.abstraction.BaseEntity
 import jakarta.persistence.EntityManager
+import jakarta.persistence.Tuple
 import mu.KotlinLogging
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
@@ -24,7 +25,7 @@ class Retriever(
 
     fun <T : BaseEntity> executeSearch(searchRequest: SearchRequest, entity: KClass<T>, page: Int, size: Int): Page<T> {
         val criteriaBuilder = entityManager.criteriaBuilder
-        val criteriaQuery = criteriaBuilder.createQuery(entity.java)
+        val criteriaQuery = criteriaBuilder.createQuery(Tuple::class.java)
         val root = criteriaQuery.from(entity.java)
         val pageable = PageRequest.of(page, size)
         log.info("Pageable info: page = {}, size = {}", pageable.pageNumber, pageable.pageSize)
@@ -43,23 +44,24 @@ class Retriever(
             }
         }
 
-        criteriaQuery.where(*predicates.toTypedArray())
+        val entityAlias = "entity"
+        val countAlias = "totalCount"
 
-        criteriaQuery.select(root).where(*predicates.toTypedArray())
+        criteriaQuery.multiselect(
+            root.alias(entityAlias),
+            criteriaBuilder.count(root).alias(countAlias)
+        ).where(*predicates.toTypedArray())
+
         val resultList = entityManager.createQuery(criteriaQuery)
             .setFirstResult(pageable.offset.toInt())
             .setMaxResults(pageable.pageSize)
             .resultList
 
-        val countQuery = criteriaBuilder.createQuery(Long::class.java)
-        countQuery.select(criteriaBuilder.count(root))
-        countQuery.where(*predicates.toTypedArray())
-        val totalCount = entityManager.createQuery(countQuery).singleResult
 
-        val pageImpl = PageImpl(resultList, pageable, totalCount)
-        log.info("PageImpl: content size = {}, total count = {}", resultList.size, totalCount)
+        val entities = resultList.map { tuple -> tuple.get(entityAlias, entity.java) }
+        val totalCount = resultList.firstOrNull()?.get(countAlias, Long::class.java) ?: 0L
 
-        return pageImpl
+        return PageImpl(entities, pageable, totalCount)
     }
 
 }
