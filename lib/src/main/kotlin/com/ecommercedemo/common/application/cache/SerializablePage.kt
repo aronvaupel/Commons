6c3c79666d39c3156cb24ed8a95971725110248a
aron <vaupelaron@gmail.com>
2024-12-27 01:53:16 +0700

    rm annotation CachingEligible.kt and attempt to store whole search result.

--- lib/src/main/kotlin/com/ecommercedemo/common/application/cache/SerializablePage.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/cache/SerializablePage.kt
@@ -1,9 +1,11 @@
 package com.ecommercedemo.common.application.cache
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import org.springframework.data.domain.Pageable
 
-data class SerializablePage<T>(
-    val content: MutableList<T>,
-    val pageable: Pageable,
-    val totalElements: Long,
+data class SerializablePage<T> @JsonCreator constructor(
+    @JsonProperty("content") val content: MutableList<T>,
+    @JsonProperty("pageNumber")val pageable: Pageable,
+    @JsonProperty("pageSize") val totalElements: Long,
 )
