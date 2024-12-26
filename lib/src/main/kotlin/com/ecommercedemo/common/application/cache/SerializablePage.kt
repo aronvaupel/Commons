56b438557749e55c2ad51984ad3a19f754ba4ed4
aron <vaupelaron@gmail.com>
2024-12-27 02:17:38 +0700

    debugging, no msg

--- lib/src/main/kotlin/com/ecommercedemo/common/application/cache/SerializablePage.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/application/cache/SerializablePage.kt
@@ -1,10 +1,11 @@
 package com.ecommercedemo.common.application.cache
 
+import com.ecommercedemo.common.model.abstraction.BaseEntity
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 
 
-data class SerializablePage<T> @JsonCreator constructor(
+data class SerializablePage<T: BaseEntity> @JsonCreator constructor(
     @JsonProperty("content") val content: MutableList<T>,
     @JsonProperty("page")val page: Int,
     @JsonProperty("size") val size: Int,
