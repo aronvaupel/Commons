5117076d77e265e651fad658b68e5ef07c6dcbc5
aron <vaupelaron@gmail.com>
2025-01-04 02:07:12 +0700

    Add ConditionalOnClass to PseudoPropertyPersistenceAdapter
    This ensures that the adapter is only loaded when JpaRepository is available in the classpath, enhancing modularity and compatibility. Additionally, the project version is updated to 6.4.10 in build.gradle.kts.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "6.4.9"
+version = "6.4.10"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/persistence/concretion/pseudoProperty/PseudoPropertyPersistenceAdapter.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/persistence/concretion/pseudoProperty/PseudoPropertyPersistenceAdapter.kt
@@ -3,8 +3,10 @@ package com.ecommercedemo.common.persistence.concretion.pseudoProperty
 import com.ecommercedemo.common.model.concretion.pseudoProperty.PseudoProperty
 import com.ecommercedemo.common.persistence.abstraction.EntityPersistenceAdapter
 import com.ecommercedemo.common.persistence.annotation.PersistenceAdapterFor
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.stereotype.Service
 
 @Service
 @PersistenceAdapterFor(PseudoProperty::class)
+@ConditionalOnClass(name = ["org.springframework.data.jpa.repository.JpaRepository"])
 open class PseudoPropertyPersistenceAdapter : EntityPersistenceAdapter<PseudoProperty>()
