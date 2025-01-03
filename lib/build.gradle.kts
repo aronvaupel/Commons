afd47313987538134fe2c23ddf27aae143c47b83
aron <vaupelaron@gmail.com>
2025-01-03 23:47:39 +0700

    Update version and add ConditionalOnClass annotations
    Bumped the library version to 6.4.6 in build.gradle.kts. Added `@ConditionalOnClass` annotations to `PseudoPropertyRepository` and `_PseudoPropertyRepository` to ensure they are only loaded when `JpaRepository` is available. This improves compatibility with Spring Data JPA.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "6.4.5"
+version = "6.4.6"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/persistence/concretion/_pseudoProperty/_PseudoPropertyRepository.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/persistence/concretion/_pseudoProperty/_PseudoPropertyRepository.kt
@@ -2,9 +2,11 @@ package com.ecommercedemo.common.persistence.concretion._pseudoProperty
 
 import com.ecommercedemo.common.model.concretion._pseudoProperty._PseudoProperty
 import com.ecommercedemo.common.persistence.abstraction.EntityRepository
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import java.util.*
 
 @Suppress("ClassName")
+@ConditionalOnClass(name = ["org.springframework.data.jpa.repository.JpaRepository"])
 interface _PseudoPropertyRepository : EntityRepository<_PseudoProperty, UUID> {
     fun findAllByEntitySimpleName(entitySimpleName: String): List<_PseudoProperty>
 }
