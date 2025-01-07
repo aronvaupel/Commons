f4ab01d8c62d8b7777e1af497c57db288cb9cf8a
aron <vaupelaron@gmail.com>
2025-01-08 02:54:01 +0700

    Enable conditional loading for controllers and bump version.
    Added `@ConditionalOnProperty` annotation to `PermissionController` and `PermissionUserAssociationController` to allow conditional loading based on `permissions` property. Updated project version from `6.6.8` to `6.6.9`.

--- lib/src/main/kotlin/com/ecommercedemo/common/controller/concretion/permission/PermissionController.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/concretion/permission/PermissionController.kt
@@ -3,10 +3,12 @@ package com.ecommercedemo.common.controller.concretion.permission
 import com.ecommercedemo.common.controller.abstraction.RestControllerTemplate
 import com.ecommercedemo.common.controller.annotation.ControllerFor
 import com.ecommercedemo.common.model.concretion.permission.Permission
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/permissions")
 @ControllerFor(Permission::class)
+@ConditionalOnProperty(name = ["permissions"], havingValue = "true", matchIfMissing = false)
 class PermissionController : RestControllerTemplate<Permission>()
--- lib/src/main/kotlin/com/ecommercedemo/common/controller/concretion/permissionuserassociation/PermissionUserAssociationController.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/concretion/permissionuserassociation/PermissionUserAssociationController.kt
@@ -3,10 +3,12 @@ package com.ecommercedemo.common.controller.concretion.permissionuserassociation
 import com.ecommercedemo.common.controller.abstraction.RestControllerTemplate
 import com.ecommercedemo.common.controller.annotation.ControllerFor
 import com.ecommercedemo.common.model.concretion.permissionuserassociation.PermissionUserAssociation
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/permission-user-associations")
 @ControllerFor(PermissionUserAssociation::class)
+@ConditionalOnProperty(name = ["permissions"], havingValue = "true", matchIfMissing = false)
 class PermissionUserAssociationController : RestControllerTemplate<PermissionUserAssociation>()
