2f23dd9588bd520f692ba3bdc1faaa1a1839e456
aron <vaupelaron@gmail.com>
2025-01-08 03:57:15 +0700

    Update version and improve repository scanning logic
    Bump the project version to 6.6.11. Refactor `RepositoryScanner` to handle entity name transformations more robustly and add logging to provide better visibility into repository discovery.

--- lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/RepositoryScanner.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/RepositoryScanner.kt
@@ -1,5 +1,6 @@
 package com.ecommercedemo.common.service.concretion
 
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.data.jpa.repository.JpaRepository
@@ -9,20 +10,28 @@ import org.springframework.stereotype.Component
 class RepositoryScanner @Autowired constructor(
     private val applicationContext: ApplicationContext,
 ) {
+    val log = KotlinLogging.logger {}
     private fun getPrimaryEntityNames(filterCondition: (String) -> Boolean): List<String> {
-        return applicationContext.getBeanNamesForType(JpaRepository::class.java)
+        val result = applicationContext.getBeanNamesForType(JpaRepository::class.java)
             .map {
-                it.replaceFirstChar { c -> c.uppercase() }
-                it.dropLast(10) 
+                if (it.startsWith("_")) {
+                    it[0] + it[1].uppercase() + it.substring(2)
+                } else {
+                    it.replaceFirstChar { c -> c.uppercaseChar() }
+                }
+
+                it.dropLast(10)
             }
             .filter(filterCondition)
+        log.info { "Found ${result.size} repositories with primary entities: $result" }
+        return result
     }
 
     fun getUpstreamEntityNames(): List<String> {
-        return getPrimaryEntityNames { !it.startsWith("_", ignoreCase = true) }
+        return getPrimaryEntityNames { !it.startsWith("_") }
     }
 
     fun getDownstreamEntityNames(): List<String> {
-        return getPrimaryEntityNames { it.startsWith("_", ignoreCase = true) }
+        return getPrimaryEntityNames { it.startsWith("_") }
     }
 }
