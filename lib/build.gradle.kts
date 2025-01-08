99250b628c83e77abda57d0e4719ef0d99bcbf1d
aron <vaupelaron@gmail.com>
2025-01-09 03:57:15 +0700

    Enhance entity change tracking with serialization support
    Refactored the `EntityChangeTracker` to improve handling of complex objects, collections, and maps by introducing custom serialization logic. Added utility methods and integrated `ObjectMapper` for consistent comparisons and serialization. Updated the project version to "6.6.23" in `build.gradle.kts`.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "6.6.22"
+version = "6.6.23"
 
 repositories {
     mavenCentral()
--- lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/EntityChangeTracker.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/service/concretion/EntityChangeTracker.kt
@@ -2,49 +2,126 @@ package com.ecommercedemo.common.service.concretion
 
 import com.ecommercedemo.common.model.abstraction.AugmentableBaseEntity
 import com.ecommercedemo.common.model.abstraction.BaseEntity
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.persistence.Embeddable
+import jakarta.persistence.Entity
 import org.springframework.stereotype.Service
 import kotlin.reflect.jvm.isAccessible
 
 @Service
+@Suppress("UNCHECKED_CAST")
 class EntityChangeTracker<T : BaseEntity>(
-    private val reflectionService: ReflectionService
-){
+    private val reflectionService: ReflectionService,
+    private val objectMapper: ObjectMapper
+) {
+
+//    fun getChangedProperties(entityBefore: T?, entityAfter: T): MutableMap<String, Any?> {
+//        val changedProperties = mutableMapOf<String, Any?>()
+//        val propertiesBefore =
+//            reflectionService.getEntityMemberProperties(entityBefore)
+//        reflectionService.getEntityMemberProperties(entityAfter)?.forEach { property ->
+//            val oldValue = entityBefore?.let { before ->
+//                val matchingProperty = propertiesBefore?.firstOrNull { it.name == property.name }
+//                    ?: throw IllegalArgumentException("Property ${property.name} not found in entity before.")
+//                matchingProperty.isAccessible = true
+//                if (before is AugmentableBaseEntity && property.name == AugmentableBaseEntity::pseudoProperties.name)
+//                    before.pseudoProperties
+//                else matchingProperty.get(before)
+//            }
+//
+//            val newValue = if (
+//                entityAfter is AugmentableBaseEntity
+//                && property.name == AugmentableBaseEntity::pseudoProperties.name
+//            )
+//                entityAfter.pseudoProperties
+//            else {
+//                property.isAccessible = true
+//                property.get(entityAfter)
+//            }
+//
+//            if (oldValue != newValue) {
+//                changedProperties[property.name] = if (property.name == AugmentableBaseEntity::pseudoProperties.name)
+//                    ObjectMapper().writeValueAsString(newValue)
+//                else
+//                    newValue
+//            }
+//
+//        }
+//        println("Changed properties: $changedProperties")
+//        return changedProperties
+//    }
 
     fun getChangedProperties(entityBefore: T?, entityAfter: T): MutableMap<String, Any?> {
         val changedProperties = mutableMapOf<String, Any?>()
-        val propertiesBefore =
-            reflectionService.getEntityMemberProperties(entityBefore)
-        reflectionService.getEntityMemberProperties(entityAfter)?.forEach { property ->
+        val propertiesBefore = reflectionService.getEntityMemberProperties(entityBefore)
+        val memberProperties = reflectionService.getEntityMemberProperties(entityAfter)
+
+        memberProperties?.forEach { property ->
             val oldValue = entityBefore?.let { before ->
                 val matchingProperty = propertiesBefore?.firstOrNull { it.name == property.name }
-                    ?: throw IllegalArgumentException("Property ${property.name} not found in entity before.")
+                    ?: throw IllegalArgumentException(
+                        "Property ${property.name} not found in entityBefore of type ${entityBefore::class.simpleName}."
+                    )
                 matchingProperty.isAccessible = true
-                if (before is AugmentableBaseEntity && property.name == AugmentableBaseEntity::pseudoProperties.name)
-                    before.pseudoProperties
-                else matchingProperty.get(before)
+                matchingProperty.get(before)
             }
 
-            val newValue = if (
-                entityAfter is AugmentableBaseEntity
-                && property.name == AugmentableBaseEntity::pseudoProperties.name
-            )
-                entityAfter.pseudoProperties
-            else {
-                property.isAccessible = true
-                property.get(entityAfter)
+            val newValue = property.run {
+                isAccessible = true
+                get(entityAfter)
             }
 
-            if (oldValue != newValue) {
-                changedProperties[property.name] = if (property.name == AugmentableBaseEntity::pseudoProperties.name)
-                    ObjectMapper().writeValueAsString(newValue)
-                else
-                    newValue
+
+            val isEqual = when {
+                oldValue == null || newValue == null -> oldValue == newValue
+                isComplexObject(oldValue) ->
+                    objectMapper.writeValueAsString(oldValue) == objectMapper.writeValueAsString(newValue)
+
+                isMap(oldValue) ->
+                    objectMapper.writeValueAsString(oldValue) == objectMapper.writeValueAsString(newValue)
+
+                isCollection(oldValue) ->
+                    objectMapper.writeValueAsString(oldValue) == objectMapper.writeValueAsString(newValue)
+
+                else -> oldValue == newValue
             }
 
+            if (!isEqual) when {
+                isComplexObject(newValue) ->
+                    changedProperties[property.name] = serializeComplexObject(newValue)
+
+                isMap(newValue) ->
+                    changedProperties[property.name] = objectMapper.writeValueAsString(newValue)
+
+                isCollection(newValue) ->
+                    changedProperties[property.name] = objectMapper.writeValueAsString(newValue)
+
+                property.name == AugmentableBaseEntity::pseudoProperties.name ->
+                    objectMapper.writeValueAsString(newValue)
+
+                else -> changedProperties[property.name] = newValue
+
+            }
         }
         println("Changed properties: $changedProperties")
         return changedProperties
+    }
+
+    private fun isComplexObject(value: Any?) =
+        value != null && (value::class.annotations.any { it is Entity || it is Embeddable } || value::class.isData)
+
+    private fun isCollection(value: Any?): Boolean = value != null && value is Collection<*>
+    private fun isMap(value: Any?): Boolean = value != null && value is Map<*, *>
+
+    private fun serializeComplexObject(value: Any?): Map<String, Any?>? {
+        return try {
+            objectMapper.convertValue(
+                value ?: return null,
+                object : TypeReference<Map<String, Any?>>() {})
+        } catch (e: Exception) {
+            mapOf("error" to "Failed to serialize object: ${e.message}")
+        }
     }
 
 }
