436a16b9bbcf3d815a44ce3afc186d501a867162
aron <vaupelaron@gmail.com>
2025-01-19 20:03:11 +0700

    rm dependencies and downgrade

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "7.4.2"
+version = "7.4.3"
 
 repositories {
     mavenCentral()
@@ -47,9 +47,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.2")
-    implementation("org.webjars:webjars-locator-core")
-    implementation("org.webjars:swagger-ui")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.7.0")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     //Fixme
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.4")
