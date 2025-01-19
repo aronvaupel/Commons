ff058a5958c6cd46e11c9337325e8bdc81e56f44
aron <vaupelaron@gmail.com>
2025-01-19 21:20:06 +0700

    downgrade springdocs to 2.6.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.github.aronvaupel"
-version = "7.4.5"
+version = "7.4.6"
 
 repositories {
     mavenCentral()
@@ -47,7 +47,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.3")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.3")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     //Fixme
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.4")
