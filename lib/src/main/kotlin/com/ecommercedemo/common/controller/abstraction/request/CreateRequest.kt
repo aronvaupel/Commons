cca2d13ab2421b5f882591feab62e5ac2ea40ff2
aron <vaupelaron@gmail.com>
2025-01-23 19:08:53 +0700

    add json creator to request (test)

--- lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/request/CreateRequest.kt
+++ lib/src/main/kotlin/com/ecommercedemo/common/controller/abstraction/request/CreateRequest.kt
@@ -1,9 +1,11 @@
 package com.ecommercedemo.common.controller.abstraction.request
 
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Request to create a new entity.")
-data class CreateRequest(
-    val entityClassName: String,
-    val properties: Map<String, Any?> = emptyMap()
+data class CreateRequest @JsonCreator constructor(
+    @JsonProperty("entityClassName") val entityClassName: String,
+    @JsonProperty("properties") val properties: Map<String, Any?> = emptyMap()
 )
